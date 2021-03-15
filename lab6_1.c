b4ee6c1a3ac2b2324110c2a891d6cefd807d241e
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-15 16:41:42 +0700

    lab6_1 up2

--- lab6_1.c
+++ lab6_1.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main ()
 {
-    int n;
+    int n,i;
     scanf("%d",&n);
     char str[n][1001];
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",str[i]);
+    }
     return 0;
 }
