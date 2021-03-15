7620683f14409ad0396e92aa7e4b2fe071c234c6
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-15 17:18:18 +0700

    lab6_3 up2

--- lab6_3.c
+++ lab6_3.c
@@ -4,9 +4,18 @@ int main ()
     int N,Q,j;
     scanf("%d %d",&N,&Q);
     int array[N],i,Z,A,B;
+    char op[2];
     for(j=0;j<N;j++)
     {
         array[j]=0;
+    }
+    for(j=0;j<Q;j++)
+    {
+        scanf("%s",op);
+        if(op[0]=='U')
+        {
+            scanf("%d %d",i,Z);
+        }
     }
     return 0;
 }
