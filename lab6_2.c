99bcf4d1438e823c22486ea3479e74fea37006fa
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-15 17:08:11 +0700

    lab6_2 up3

--- lab6_2.c
+++ lab6_2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main ()
 {
-    int n,q,a,i;
+    int n,q,a,i,cor=0;
     scanf("%d",&n);
-    int Q[n],A[n];
+    int Q[n],A[n],wor=n;
     scanf("%d %d",&q,&a);
     for(i=n-1;i>=0;i--)
     {
@@ -12,5 +12,14 @@ int main ()
         q/=10;
         a/=10;
     }
+    for(i=0;i<n;i++)
+    {
+        if(Q[i]==A[i])
+        {
+            cor++;
+            wor--;
+        }
+    }
+    printf("%d %d",cor,wor);
     return 0;
 }
