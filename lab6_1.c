92381be5c2bf9e5d1aa6ee2d838c52d40e5c03a3
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-15 18:59:44 +0700

    lab6_1 fix2
    ล้มเหลว ได้80เท่าเดิม

--- lab6_1.c
+++ lab6_1.c
@@ -2,11 +2,11 @@
 #include <string.h>
 int main ()
 {
-    int n,i,j,k;
+    int n,i,j,k,l;
     scanf("%d",&n);
     char str[n][1001],tmp[1001];
     int re[n];
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
         scanf("%s",str[i]);
         re[i]=0;
@@ -22,23 +22,44 @@ int main ()
         {
             if(str[j][0]<str[i][0])
             {
-                strcpy(tmp,str[j]); //a=x[j];
+                strcpy(tmp,str[j]);
                 for(k=j; k>i; k--)
                 {
-                    strcpy(str[k],str[k-1]);//x[k]=x[k-1];
+                    strcpy(str[k],str[k-1]);
                 }
-                strcpy(str[k],tmp);//x[k]=a;
+                strcpy(str[k],tmp);
             }
         }
     }
-    for(i=0;i<n;i++)
+    /*for(l=0; l<5; l++)
+    {
+        for(j=1; j<n; j++)
+        {
+            for(i=0; i<j; i++)
+            {
+                if(str[j][l-1]==str[i][l-1])
+                {
+                    if(str[j][l]<str[i][l])
+                    {
+                        strcpy(tmp,str[j]);
+                        for(k=j; k>i; k--)
+                        {
+                            strcpy(str[k],str[k-1]);
+                        }
+                        strcpy(str[k],tmp);
+                    }
+                }
+            }
+        }
+    }*/
+    for(i=0; i<n; i++)
     {
         if(re[i]==1)
         {
             str[i][0]+=32;
         }
     }
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
         printf("%s\n",str[i]);
     }
