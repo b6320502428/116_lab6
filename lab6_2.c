827be109374c652bcfba594dca91c4791adef0a8
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-15 17:03:32 +0700

    lab6_2 up2

--- lab6_2.c
+++ lab6_2.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 int main ()
 {
-    int n,q,a;
+    int n,q,a,i;
     scanf("%d",&n);
     int Q[n],A[n];
     scanf("%d %d",&q,&a);
+    for(i=n-1;i>=0;i--)
+    {
+        Q[i]=q%10;
+        A[i]=a%10;
+        q/=10;
+        a/=10;
+    }
     return 0;
 }
