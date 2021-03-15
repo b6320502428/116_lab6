d37cd868d6097679f08f3ac5d3240255889c1b80
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-15 18:47:51 +0700

    lab 6_1 fix1
    แก้เคสรอบที่1

--- lab6_1.c
+++ lab6_1.c
@@ -5,12 +5,15 @@ int main ()
     int n,i,j,k;
     scanf("%d",&n);
     char str[n][1001],tmp[1001];
+    int re[n];
     for(i=0;i<n;i++)
     {
         scanf("%s",str[i]);
+        re[i]=0;
         if(str[i][0]>96&&str[i][0]<123)
         {
             str[i][0]-=32;
+            re[i]++;
         }
     }
     for(j=1; j<n; j++)
@@ -26,6 +29,13 @@ int main ()
                 }
                 strcpy(str[k],tmp);//x[k]=a;
             }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(re[i]==1)
+        {
+            str[i][0]+=32;
         }
     }
     for(i=0;i<n;i++)
