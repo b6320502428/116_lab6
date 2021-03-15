1f43117c000d723c8a8f3520ff273ffe03bea85f
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-15 17:38:16 +0700

    lab6_4 up2

--- lab6_4.c
+++ lab6_4.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 int main ()
 {
-    int n,i;
+    int n,i,max,win1=0,win2;
     scanf("%d",&n);
     int num[n][2],score[n];
     for(i=0;i<n;i++)
     {
         score[i]=0;
+    }
+    for(i=0;i<n;i++)
+    {
+        scanf("%d %d",&num[i][0],&num[i][1]);
+        score[num[i][0]]++;
+        score[num[i][1]]++;
+    }
+    max=score[0];
+    for(i=1;i<n;i++)
+    {
+        if(max<score[i])
+        {
+            max=score[i];
+            win1=i;
+        }
     }
     return 0;
 }
