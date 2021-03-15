8a58943e252430a02af236828e7128acd6a8a097
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-15 16:45:19 +0700

    lab6_1 up3

--- lab6_1.c
+++ lab6_1.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 int main ()
 {
     int n,i;
     scanf("%d",&n);
-    char str[n][1001];
+    char str[n][1001],tmp[1001];
     for(i=0;i<n;i++)
     {
         scanf("%s",str[i]);
+        if(str[i][0]>96&&str[i][0]<123)
+        {
+            str[i][0]-=32;
+        }
     }
     return 0;
 }
