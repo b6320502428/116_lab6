7df700660eabb62aea80b006f1546c1652dafa7d
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-15 16:50:44 +0700

    lab6_1 up4
    ก็อปโค็ดinsertion sortจากงานที่เคยทำไปแล้ว

--- lab6_1.c
+++ lab6_1.c
@@ -13,5 +13,24 @@ int main ()
             str[i][0]-=32;
         }
     }
+    /*for(j=1; j<n; j++)
+    {
+        for(i=0; i<j; i++)
+        {
+            if(x[j]<x[i])
+            {
+                a=x[j];
+                for(k=j; k>i; k--)
+                {
+                    x[k]=x[k-1];
+                }
+                x[k]=a;
+            }
+        }
+    }*/
+    /*for(i=0;i<n;i++)
+    {
+        printf("%s",str[i]);
+    }*/
     return 0;
 }
