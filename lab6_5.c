9f62e2cc8710b7f4235a884d58ab66ce2a0b22f8
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-15 18:04:09 +0700

    lab6_5 up1

--- lab6_5.c
+++ lab6_5.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 int main ()
 {
-    int w,h;
-
+    int w,h,i,j,t=0;
+    scanf("%d %d",&w,&h);
+    int n[h][w];
+    for(i=0; i<h; i++)
+    {
+        for(j=0; j<w; j++)
+        {
+            scanf("%d",&n[i][j]);
+            t+=n[i][j];
+        }
+    }
     return 0;
 }
