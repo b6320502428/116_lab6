f26a7c8ec7df9b80a71417c866094392a7ad5a31
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-03-15 18:25:09 +0700

    lab6_6 up1

--- lab6_6.c
+++ lab6_6.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 int main ()
 {
-    int n;
+    int n,i,j;
     char op[251];
     scanf("%d",&n);
     char array[n][n];
+    scanf("%s",op);
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            array[i][j]='.';
+        }
+    }
+    for(i=0; i<strlen(op); i++)
+    {
+
+    }
     return 0;
 }
